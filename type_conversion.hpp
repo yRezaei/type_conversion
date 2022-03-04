28fd956d6ce4d6ba81274b4cd012b0692759758b
Yashar A. Rezaei <yashar.a.rezaei@gmail.com>
2022-03-04 22:16:47 +0100

    boolean specialization added

--- type_conversion.hpp
+++ type_conversion.hpp
@@ -1,228 +1,258 @@
-#ifndef C554D4CB_CDE3_44BE_A2B1_01A549A3C733
-#define C554D4CB_CDE3_44BE_A2B1_01A549A3C733
+#ifndef D8871FD6_5FDA_4344_96B3_1ACA566E3C68
+#define D8871FD6_5FDA_4344_96B3_1ACA566E3C68
 
 #include <cstdlib>
 #include <string>
 #include <stdexcept>
+#include <exception>
+#include <utility>
 
-namespace convert
-{
-    template <typename T_return, typename T_arg>
-    T_return to(T_arg value) = delete;
+/*
+Description:    A Header only templated functions to convert basic types to and from std::string
+Author:         Yashar Abbasalizadeh Rezaei
+Email:          yashar.rezaei@atlascopco.com
+*/
+namespace convert {
+template <typename T_return, typename T_arg>
+T_return to(T_arg value) = delete;
 
-    // Numerical specialization to std::string
-    template <>
-    inline std::string to(int value) { return std::to_string(value); };
+// Numerical specialization to std::string
+template <>
+inline std::string to(std::int16_t value) {
+  return std::to_string(static_cast<std::int32_t>(value));
+};
 
-    template <>
-    inline std::string to(long value) { return std::to_string(value); };
+template <>
+inline std::string to(int value) {
+  return std::to_string(value);
+};
 
-    template <>
-    inline std::string to(long long value) { return std::to_string(value); };
+template <>
+inline std::string to(long value) {
+  return std::to_string(value);
+};
 
-    template <>
-    inline std::string to(unsigned int value) { return std::to_string(value); };
+template <>
+inline std::string to(long long value) {
+  return std::to_string(value);
+};
 
-    template <>
-    inline std::string to(unsigned long value) { return std::to_string(value); };
+template <>
+inline std::string to(std::uint16_t value) {
+  return std::to_string(static_cast<std::uint32_t>(value));
+};
 
-    template <>
-    inline std::string to(unsigned long long value) { return std::to_string(value); };
+template <>
+inline std::string to(unsigned int value) {
+  return std::to_string(value);
+};
 
-    template <>
-    inline std::string to(float value) { return std::to_string(value); };
+template <>
+inline std::string to(unsigned long value) {
+  return std::to_string(value);
+};
 
-    template <>
-    inline std::string to(double value) { return std::to_string(value); };
+template <>
+inline std::string to(unsigned long long value) {
+  return std::to_string(value);
+};
 
-    template <>
-    inline std::string to(long double value) { return std::to_string(value); };
+template <>
+inline std::string to(float value) {
+  return std::to_string(value);
+};
 
-    // std::string const& specialization to numerical
-    template <>
-    inline int to(std::string const &value)
-    {
-        std::size_t pos{};
-        auto num = std::stoi(value, &pos);
-        if (value.size() != pos)
-            throw std::invalid_argument("Te given string doesn't represend an int!");
-        return num;
-    };
+template <>
+inline std::string to(double value) {
+  return std::to_string(value);
+};
 
-    template <>
-    inline long to(std::string const &value)
-    {
-        std::size_t pos{};
-        auto num = std::stol(value, &pos);
-        if (value.size() != pos)
-            throw std::invalid_argument("Te given string doesn't represend a long!");
-    };
+template <>
+inline std::string to(long double value) {
+  return std::to_string(value);
+};
 
-    template <>
-    inline long long to(std::string const &value)
-    {
-        std::size_t pos{};
-        auto num = std::stoll(value, &pos);
-        if (value.size() != pos)
-            throw std::invalid_argument("Te given string doesn't represend a long long!");
-        return num;
-    };
+// std::string const& specialization to numerical
+template <>
+inline bool to(std::string const& value) {
+  return (value.size() > 1 ? (value == "True" || value == "TRUE" || value == "true" ? true : false)
+                           : (value == "1" ? true : false));
+};
 
-    template <>
-    inline unsigned int to(std::string const &value)
-    {
-        std::size_t pos{};
-        unsigned long numl = stoul(value, &pos, 10);
-        if (value.size() != pos)
-            throw std::invalid_argument("Te given string doesn't represend an unsigned int!");
-        auto num = static_cast<unsigned int>(numl);
-        if (num != numl)
-            throw std::out_of_range("The given string can not be converted to unsigned int.");
-        return num;
-    };
+template <>
+inline int to(std::string const& value) {
+  std::size_t pos{};
+  auto num = std::stoi(value, &pos);
+  if (value.size() != pos)
+    throw std::invalid_argument(std::string("The given string\"") + value + "\" doesn't represent an int!");
+  return num;
+};
 
-    template <>
-    inline unsigned long to(std::string const &value)
-    {
-        std::size_t pos{};
-        auto num = std::stoul(value, &pos);
-        if (value.size() != pos)
-            throw std::invalid_argument("Te given string doesn't represend an unsigned long!");
-        return num;
-    };
+template <>
+inline long to(std::string const& value) {
+  std::size_t pos{};
+  auto num = std::stol(value, &pos);
+  if (value.size() != pos)
+    throw std::invalid_argument(std::string("The given string\"") + value + "\" doesn't represent a long!");
+  return num;
+};
 
-    template <>
-    inline unsigned long long to(std::string const &value)
-    {
-        std::size_t pos{};
-        auto num = std::stoull(value, &pos);
-        if (value.size() != pos)
-            throw std::invalid_argument("Te given string doesn't represend an unsigned long long!");
-        return num;
-    };
+template <>
+inline long long to(std::string const& value) {
+  std::size_t pos{};
+  auto num = std::stoll(value, &pos);
+  if (value.size() != pos)
+    throw std::invalid_argument(std::string("The given string\"") + value + "\" doesn't represent a long long!");
+  return num;
+};
 
-    template <>
-    inline float to(std::string const &value)
-    {
-        std::size_t pos{};
-        auto num = std::stof(value, &pos);
-        if (value.size() != pos)
-            throw std::invalid_argument("Te given string doesn't represend a float!");
-        return num;
-    };
+template <>
+inline unsigned int to(std::string const& value) {
+  std::size_t pos{};
+  unsigned long numl = stoul(value, &pos, 10);
+  if (value.size() != pos)
+    throw std::invalid_argument(std::string("The given string\"") + value + "\" doesn't represent an unsigned int!");
+  auto num = static_cast<unsigned int>(numl);
+  if (num != numl) throw std::out_of_range("The given string can not be converted to unsigned int.");
+  return num;
+};
 
-    template <>
-    inline double to(std::string const &value)
-    {
-        std::size_t pos{};
-        auto num = std::stod(value, &pos);
-        if (value.size() != pos)
-            throw std::invalid_argument("Te given string doesn't represend a double!");
-        return num;
-    };
+template <>
+inline unsigned long to(std::string const& value) {
+  std::size_t pos{};
+  auto num = std::stoul(value, &pos);
+  if (value.size() != pos)
+    throw std::invalid_argument(std::string("The given string\"") + value + "\" doesn't represent an unsigned long!");
+  return num;
+};
 
-    template <>
-    inline long double to(std::string const &value)
-    {
-        std::size_t pos{};
-        auto num = std::stold(value, &pos);
-        if (value.size() != pos)
-            throw std::invalid_argument("Te given string doesn't represend a long double!");
-        return num;
-    };
+template <>
+inline unsigned long long to(std::string const& value) {
+  std::size_t pos{};
+  auto num = std::stoull(value, &pos);
+  if (value.size() != pos)
+    throw std::invalid_argument(std::string("The given string\"") + value +
+                                "\" doesn't represent an unsigned long long!");
+  return num;
+};
 
-    // std::string specialization to numerical
-    template <>
-    inline int to(std::string value)
-    {
-        std::size_t pos{};
-        auto num = std::stoi(value, &pos);
-        if (value.size() != pos)
-            throw std::invalid_argument("Te given string doesn't represend an int!");
-        return num;
-    };
+template <>
+inline float to(std::string const& value) {
+  std::size_t pos{};
+  auto num = std::stof(value, &pos);
+  if (value.size() != pos)
+    throw std::invalid_argument(std::string("The given string\"") + value + "\" doesn't represent a float!");
+  return num;
+};
 
-    template <>
-    inline long to(std::string value)
-    {
-        std::size_t pos{};
-        auto num = std::stol(value, &pos);
-        if (value.size() != pos)
-            throw std::invalid_argument("Te given string doesn't represend a long!");
-    };
+template <>
+inline double to(std::string const& value) {
+  std::size_t pos{};
+  auto num = std::stod(value, &pos);
+  if (value.size() != pos)
+    throw std::invalid_argument(std::string("The given string\"") + value + "\" doesn't represent a double!");
+  return num;
+};
 
-    template <>
-    inline long long to(std::string value)
-    {
-        std::size_t pos{};
-        auto num = std::stoll(value, &pos);
-        if (value.size() != pos)
-            throw std::invalid_argument("Te given string doesn't represend a long long!");
-        return num;
-    };
+template <>
+inline long double to(std::string const& value) {
+  std::size_t pos{};
+  auto num = std::stold(value, &pos);
+  if (value.size() != pos)
+    throw std::invalid_argument(std::string("The given string\"") + value + "\" doesn't represent a long double!");
+  return num;
+};
 
-    template <>
-    inline unsigned int to(std::string value)
-    {
-        std::size_t pos{};
-        unsigned long numl = stoul(value, &pos, 10);
-        if (value.size() != pos)
-            throw std::invalid_argument("Te given string doesn't represend an unsigned int!");
-        auto num = static_cast<unsigned int>(numl);
-        if (num != numl)
-            throw std::out_of_range("The given string can not be converted to unsigned int.");
-        return num;
-    };
+// std::string specialization to numerical
+template <>
+inline bool to(std::string value) {
+  return (value.size() > 1 ? (value == "True" || value == "TRUE" || value == "true" ? true : false)
+                           : (value == "1" ? true : false));
+};
 
-    template <>
-    inline unsigned long to(std::string value)
-    {
-        std::size_t pos{};
-        auto num = std::stoul(value, &pos);
-        if (value.size() != pos)
-            throw std::invalid_argument("Te given string doesn't represend an unsigned long!");
-        return num;
-    };
+template <>
+inline int to(std::string value) {
+  std::size_t pos{};
+  auto num = std::stoi(value, &pos);
+  if (value.size() != pos)
+    throw std::invalid_argument(std::string("The given string\"") + value + "\" doesn't represent an int!");
+  return num;
+};
 
-    template <>
-    inline unsigned long long to(std::string value)
-    {
-        std::size_t pos{};
-        auto num = std::stoull(value, &pos);
-        if (value.size() != pos)
-            throw std::invalid_argument("Te given string doesn't represend an unsigned long long!");
-        return num;
-    };
+template <>
+inline long to(std::string value) {
+  std::size_t pos{};
+  auto num = std::stol(value, &pos);
+  if (value.size() != pos)
+    throw std::invalid_argument(std::string("The given string\"") + value + "\" doesn't represent a long!");
+  return num;
+};
 
-    template <>
-    inline float to(std::string value)
-    {
-        std::size_t pos{};
-        auto num = std::stof(value, &pos);
-        if (value.size() != pos)
-            throw std::invalid_argument("Te given string doesn't represend a float!");
-        return num;
-    };
+template <>
+inline long long to(std::string value) {
+  std::size_t pos{};
+  auto num = std::stoll(value, &pos);
+  if (value.size() != pos)
+    throw std::invalid_argument(std::string("The given string\"") + value + "\" doesn't represent a long long!");
+  return num;
+};
 
-    template <>
-    inline double to(std::string value)
-    {
-        std::size_t pos{};
-        auto num = std::stod(value, &pos);
-        if (value.size() != pos)
-            throw std::invalid_argument("Te given string doesn't represend a double!");
-        return num;
-    };
+template <>
+inline unsigned int to(std::string value) {
+  std::size_t pos{};
+  unsigned long numl = stoul(value, &pos, 10);
+  if (value.size() != pos)
+    throw std::invalid_argument(std::string("The given string\"") + value + "\" doesn't represent an unsigned int!");
+  auto num = static_cast<unsigned int>(numl);
+  if (num != numl) throw std::out_of_range("The given string can not be converted to unsigned int.");
+  return num;
+};
 
-    template <>
-    inline long double to(std::string value)
-    {
-        std::size_t pos{};
-        auto num = std::stold(value, &pos);
-        if (value.size() != pos)
-            throw std::invalid_argument("Te given string doesn't represend a long double!");
-        return num;
-    };
-}
+template <>
+inline unsigned long to(std::string value) {
+  std::size_t pos{};
+  auto num = std::stoul(value, &pos);
+  if (value.size() != pos)
+    throw std::invalid_argument(std::string("The given string\"") + value + "\" doesn't represent an unsigned long!");
+  return num;
+};
 
-#endif /* C554D4CB_CDE3_44BE_A2B1_01A549A3C733 */
+template <>
+inline unsigned long long to(std::string value) {
+  std::size_t pos{};
+  auto num = std::stoull(value, &pos);
+  if (value.size() != pos)
+    throw std::invalid_argument(std::string("The given string\"") + value +
+                                "\" doesn't represent an unsigned long long!");
+  return num;
+};
+
+template <>
+inline float to(std::string value) {
+  std::size_t pos{};
+  auto num = std::stof(value, &pos);
+  if (value.size() != pos)
+    throw std::invalid_argument(std::string("The given string\"") + value + "\" doesn't represent a float!");
+  return num;
+};
+
+template <>
+inline double to(std::string value) {
+  std::size_t pos{};
+  auto num = std::stod(value, &pos);
+  if (value.size() != pos)
+    throw std::invalid_argument(std::string("The given string\"") + value + "\" doesn't represent a double!");
+  return num;
+};
+
+template <>
+inline long double to(std::string value) {
+  std::size_t pos{};
+  auto num = std::stold(value, &pos);
+  if (value.size() != pos)
+    throw std::invalid_argument(std::string("The given string\"") + value + "\" doesn't represent a long double!");
+  return num;
+};
+}  // namespace convert
+
+#endif /* D8871FD6_5FDA_4344_96B3_1ACA566E3C68 */
