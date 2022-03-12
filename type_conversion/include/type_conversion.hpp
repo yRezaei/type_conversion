f0d766f8d8b96830f47b6491046ab56a146b8fe0
Yashar A. Rezaei <yashar.a.rezaei@gmail.com>
2022-03-12 19:31:57 +0100

    range error check added to char const* conversion

--- type_conversion/include/type_conversion.hpp
+++ type_conversion/include/type_conversion.hpp
@@ -1,12 +1,13 @@
 #ifndef F829904E_F0F8_4832_98A1_1E614847C8E0
 #define F829904E_F0F8_4832_98A1_1E614847C8E0
 
-#include <cstdlib>
 #include <string>
 #include <stdexcept>
 #include <exception>
 #include <utility>
 #include <cstring>
+#include <cerrno>
+#include <cstdlib>
 
 using namespace std::string_literals;
 
@@ -53,7 +54,7 @@ namespace convert
             return false;
         else if (value.compare("true"s) == 0 || value.compare("True"s) == 0 || value.compare("TRUE"s) == 0)
             return true;
-        else if (value.compare("false"s) == 0 || value.compare("False"s) == 0|| value.compare("FALSE"s) == 0)
+        else if (value.compare("false"s) == 0 || value.compare("False"s) == 0 || value.compare("FALSE"s) == 0)
             return false;
         else
             throw std::invalid_argument(std::string("The given string \"") + value + "\" doesn't represent any known boolean representation!");
@@ -265,127 +266,149 @@ namespace convert
 
     // char const* to numerical
     template <>
-    inline std::int16_t to(char const*value)
+    inline std::int16_t to(char const *value)
     {
-        char* value_end;
+        char *value_end;
         auto num = static_cast<std::int16_t>(std::strtol(value, &value_end, 10));
         if (std::strlen(value) != static_cast<std::size_t>(value_end - value))
             throw std::invalid_argument(std::string("The given byte string \"") + value + "\" doesn't represent any known short representation!");
+        if (errno == ERANGE)
+            throw std::out_of_range(std::string("The given string \"") + value + "\" doesn't represent a long double!");
 
         return num;
     }
 
     template <>
-    inline std::int32_t to(char const*value)
+    inline std::int32_t to(char const *value)
     {
-        char* value_end;
+        char *value_end;
         auto num = static_cast<std::int32_t>(std::strtol(value, &value_end, 10));
         if (std::strlen(value) != static_cast<std::size_t>(value_end - value))
             throw std::invalid_argument(std::string("The given byte string \"") + value + "\" doesn't represent any known int representation!");
+        if (errno == ERANGE)
+            throw std::out_of_range(std::string("The given string \"") + value + "\" doesn't represent a long double!");
 
         return num;
     }
 
     template <>
-    inline std::int64_t to(char const*value)
+    inline std::int64_t to(char const *value)
     {
-        char* value_end;
+        char *value_end;
         auto num = std::strtol(value, &value_end, 10);
         if (std::strlen(value) != static_cast<std::size_t>(value_end - value))
             throw std::invalid_argument(std::string("The given byte string \"") + value + "\" doesn't represent any known long representation!");
+        if (errno == ERANGE)
+            throw std::out_of_range(std::string("The given string \"") + value + "\" doesn't represent a long double!");
 
         return num;
     }
 
     template <>
-    inline long long to(char const*value)
+    inline long long to(char const *value)
     {
-        char* value_end;
+        char *value_end;
         auto num = std::strtoll(value, &value_end, 10);
         if (std::strlen(value) != static_cast<std::size_t>(value_end - value))
             throw std::invalid_argument(std::string("The given byte string \"") + value + "\" doesn't represent any known long long representation!");
+        if (errno == ERANGE)
+            throw std::out_of_range(std::string("The given string \"") + value + "\" doesn't represent a long double!");
 
         return num;
     }
 
     template <>
-    inline std::uint16_t to(char const*value)
+    inline std::uint16_t to(char const *value)
     {
-        char* value_end;
+        char *value_end;
         auto num = static_cast<std::uint16_t>(std::strtoul(value, &value_end, 10));
         if (std::strlen(value) != static_cast<std::size_t>(value_end - value))
             throw std::invalid_argument(std::string("The given byte string \"") + value + "\" doesn't represent any known unsigned short representation!");
+        if (errno == ERANGE)
+            throw std::out_of_range(std::string("The given string \"") + value + "\" doesn't represent a long double!");
 
         return num;
     }
 
     template <>
-    inline std::uint32_t to(char const*value)
+    inline std::uint32_t to(char const *value)
     {
-        char* value_end;
+        char *value_end;
         auto num = static_cast<std::uint32_t>(std::strtoul(value, &value_end, 10));
         if (std::strlen(value) != static_cast<std::size_t>(value_end - value))
             throw std::invalid_argument(std::string("The given byte string \"") + value + "\" doesn't represent any known unsigned int representation!");
+        if (errno == ERANGE)
+            throw std::out_of_range(std::string("The given string \"") + value + "\" doesn't represent a long double!");
 
         return num;
     }
 
     template <>
-    inline std::uint64_t to(char const*value)
+    inline std::uint64_t to(char const *value)
     {
-        char* value_end;
+        char *value_end;
         auto num = std::strtoul(value, &value_end, 10);
         if (std::strlen(value) != static_cast<std::size_t>(value_end - value))
             throw std::invalid_argument(std::string("The given byte string \"") + value + "\" doesn't represent any known unsigned long representation!");
+        if (errno == ERANGE)
+            throw std::out_of_range(std::string("The given string \"") + value + "\" doesn't represent a long double!");
 
         return num;
     }
 
     template <>
-    inline unsigned long long to(char const*value)
+    inline unsigned long long to(char const *value)
     {
-        char* value_end;
+        char *value_end;
         auto num = std::strtoull(value, &value_end, 10);
         if (std::strlen(value) != static_cast<std::size_t>(value_end - value))
             throw std::invalid_argument(std::string("The given byte string \"") + value + "\" doesn't represent any known unsigned long long representation!");
+        if (errno == ERANGE)
+            throw std::out_of_range(std::string("The given string \"") + value + "\" doesn't represent a long double!");
 
         return num;
     }
 
     template <>
-    inline float to(char const*value)
+    inline float to(char const *value)
     {
-        char* value_end;
+        char *value_end;
         auto num = std::strtof(value, &value_end);
         if (std::strlen(value) != static_cast<std::size_t>(value_end - value))
             throw std::invalid_argument(std::string("The given byte string \"") + value + "\" doesn't represent any known float representation!");
+        if (errno == ERANGE)
+            throw std::out_of_range(std::string("The given string \"") + value + "\" doesn't represent a long double!");
 
         return num;
     }
 
     template <>
-    inline double to(char const*value)
+    inline double to(char const *value)
     {
-        char* value_end;
+        char *value_end;
         auto num = std::strtod(value, &value_end);
         if (std::strlen(value) != static_cast<std::size_t>(value_end - value))
             throw std::invalid_argument(std::string("The given byte string \"") + value + "\" doesn't represent any known double representation!");
+        if (errno == ERANGE)
+            throw std::out_of_range(std::string("The given string \"") + value + "\" doesn't represent a long double!");
 
         return num;
     }
 
     template <>
-    inline long double to(char const*value)
+    inline long double to(char const *value)
     {
-        char* value_end;
+        char *value_end;
         auto num = std::strtold(value, &value_end);
         if (std::strlen(value) != static_cast<std::size_t>(value_end - value))
             throw std::invalid_argument(std::string("The given byte string \"") + value + "\" doesn't represent any known long double representation!");
+        if (errno == ERANGE)
+            throw std::out_of_range(std::string("The given string \"") + value + "\" doesn't represent a long double!");
 
         return num;
     }
 
-     // Specialization from std::string to numerical
+    // Specialization from std::string to numerical
     template <>
     inline std::int16_t to(std::string value)
     {
